27315a01650d7f6dbc4d422839f200530603905c
odinellefsen <odin@ellefsen.fo>
2026-05-12 15:15:53 +0100

    feat: specify input device index for MacBook Pro microphone in mic_test.py to enhance audio input configuration

--- text-to-speech/mic_test.py
+++ text-to-speech/mic_test.py
@@ -13,6 +13,7 @@ stream = mic.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=16000,
                 input=True,
+                input_device_index=2,  # MacBook Pro Microphone
                 frames_per_buffer=8192)
 stream.start_stream()
 print("Listening... (speak now, Ctrl+C to stop)")
