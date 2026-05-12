734bf026e053d732b45da4b8287608be00f9d792
odinellefsen <odin@ellefsen.fo>
2026-05-12 15:01:02 +0100

    refactor: update model path in mic_test.py for better portability and add exception handling for audio stream overflow

--- text-to-speech/mic_test.py
+++ text-to-speech/mic_test.py
@@ -1,7 +1,9 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
+import os
 
-model = Model(r"C:/Users/odin/repos/Butterbot/llm_test/vosk/vosk-model-small-en-us-0.15")
+model_path = os.path.join(os.path.dirname(__file__), "vosk", "vosk-model-small-en-us-0.15")
+model = Model(model_path)
 recognizer = KaldiRecognizer(model, 16000)
 
 mic = pyaudio.PyAudio()
@@ -10,13 +12,12 @@ stream = mic.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=16000,
                 input=True,
-                input_device_index=2,  # This targets the microphone at index 2
                 frames_per_buffer=8192)
 stream.start_stream()
 
 
 while True:
-    data = stream.read(4096)
+    data = stream.read(4096, exception_on_overflow=False)
     if recognizer.AcceptWaveform(data):
         text = recognizer.Result()
         print(text[14:-3])
